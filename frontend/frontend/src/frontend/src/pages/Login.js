6af3caf545fe0abc21f6d89c5215c42768c899ea
AdePiano <adaneera538@gmail.com>
2026-06-22 02:37:04 +0300

    Create Login.js

---
+++ frontend/frontend/src/frontend/src/pages/Login.js
@@ -0,0 +1,12 @@
+function Login() {
+  return (
+    <div>
+      <h2>Student Login</h2>
+      <input placeholder="Student ID" />
+      <input placeholder="Password" type="password" />
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
